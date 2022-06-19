c9a0f18a40921f277d03d86645f76d321f07999e
RicardoCaselati <ricardocaselati@gmail.com>
2022-06-19 12:17:27 -0300

    Portfólio

---
+++ activeNav.js
@@ -0,0 +1,15 @@
+function navbarSelectedItem() {
+  const objectOfNav = document.querySelectorAll('.nav-item');
+  document.addEventListener('click', function (event) {
+    if (event.target.classList.contains("nav-link")) {
+      if (event.target.classList.contains("active")) {
+        return console.log('estou aqui')
+      } else {
+        let activeItem = document.getElementsByClassName("active")[0];
+        activeItem.classList.remove("active");
+        event.target.classList.add("active");
+      }
+      }
+  }, false);
+}
+navbarSelectedItem()
